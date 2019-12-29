151afbb099d778336605029d346378fdcdf73be6
Majlo <milosz.karlowicz@gmail.com>
2019-12-29 20:58:04 +0100

    Comments edition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 
 public class Main {
 /*  TODO
-ZROBIONE - dopisac resetowanie lelvelu w trakcie gry jezeli nie idzie grac dalej lub jest bug
--wiecej leveli
--dodac skrzynke do przesuwania
-ZROBIONE - wyjscie do menu w trakcie gry klawiszem p
-ZROBIONE - dodac zerowanie ukonczenia poszczegolnych leveli
-ZROBIONE - wyjscie z etapu wybierania leveli tak zeby zadnego nie wybrac
+
 * */
     public static void main(String[] args) {
         Game game = new Game(5);
